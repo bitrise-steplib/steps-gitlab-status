341a6f87065cfbfb7063cf985966d0b994fbb805
Usman <akeju00+github@gmail.com>
2020-05-21 12:36:16 +0200

    Restores support for repos in subgroups (#22)
    Fixes bitrise-steplib/steps-gitlab-status#19

--- main_test.go
+++ main_test.go
@@ -12,17 +12,23 @@ func Test_getRepo(t *testing.T) {
 		wantErr bool
 	}{
 		{"https", "https://github.com/owner/repository.git", "owner/repository", false},
+		{"https with subgroup", "https://github.com/group/subgroup/subsubgroup/repository.git", "group/subgroup/subsubgroup/repository", false},
 		{"https with port", "https://github.com:443/owner/repository.git", "owner/repository", false},
 		{"https slash at end", "https://github.com/owner/repository.git/", "owner/repository", false},
 		{"https no .git", "https://github.com/owner/repository", "owner/repository", false},
-		{"https not .git with slash at end", "https://github.com/owner/repository/", "owner/repository", false},
+		{"https no .git with slash at end", "https://github.com/owner/repository/", "owner/repository", false},
 		{"https custom domain", "https://gitlab.custom.com/owner/repository.git", "owner/repository", false},
 		{"https with basic auth", "https://username:token@github.com/owner/repository.git", "owner/repository", false},
+		{"https with port and basic auth", "https://username:token@github.com:443/owner/repository.git", "owner/repository", false},
 		{"ssh without scheme", "user@github.com:owner/repository.git", "owner/repository", false},
 		{"ssh without scheme, no user", "github.com:owner/repository.git", "owner/repository", false},
+		{"ssh without scheme with subgroups", "user@gitlab.com:group/subgroup/subsubgroup/repository.git", "group/subgroup/subsubgroup/repository", false},
 		{"ssh with scheme without port", "ssh://user@github.com/owner/repository.git", "owner/repository", false},
 		{"ssh with scheme without port, no user", "ssh://github.com/owner/repository.git", "owner/repository", false},
 		{"ssh with scheme with port", "ssh://user@github.com:22/owner/repository.git", "owner/repository", false},
+		{"ssh with scheme with subgroups", "ssh://user@gitlab.com/group/subgroup/subsubgroup/repository.git", "group/subgroup/subsubgroup/repository", false},
+		{"ssh with scheme, invalid repo", "ssh://user@gitlab.com/repository.git", "", false},
+		{"ssh without scheme, invalid repo", "user@gitlab.com:repository.git", "", false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
