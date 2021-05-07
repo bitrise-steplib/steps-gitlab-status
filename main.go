3d30d8ee4c14dbfcc288d4afb6078013df30a7d3
Daniel Serleg <daniel.serleg@bitrise.io>
2021-05-07 11:44:18 +0200

    Fix the missing content-type header in the request

--- main.go
+++ main.go
@@ -77,6 +77,7 @@ func sendStatus(cfg config) error {
 		return err
 	}
 	req.Header.Add("PRIVATE-TOKEN", cfg.PrivateToken)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
